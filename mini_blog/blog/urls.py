e403cf3bcf155ea66a631169c4fcf5bf2f79243c
NikolaiNikitenok <k89095867511@gmail.com>
2023-03-19 23:11:24 +0300

    create blog

---
+++ mini_blog/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views # "." - текущий каталог
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]
---
+++ mini_blog/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Post
+# from django.http import HttpResponse
+
+def home(request):
+    context = {
+		'posts': Post.objects.all() # Передаем словарь с переменными в html
+	}
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
