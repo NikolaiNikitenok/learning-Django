f8f8afe208fc6179ee34c9db679d4008d9375343
NikolaiNikitenok <k89095867511@gmail.com>
2023-03-21 22:40:52 +0300

    photo for profile

--- mini_blog/users/views.py
+++ mini_blog/users/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
