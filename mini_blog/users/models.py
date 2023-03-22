c19681df89dc7d46cb5ea08071f4a52d882c01e2
NikolaiNikitenok <k89095867511@gmail.com>
2023-03-22 23:42:01 +0300

    added create posts, update posts and delete

--- mini_blog/users/models.py
+++ mini_blog/users/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
+def crop_center(pil_img, crop_width: int, crop_height: int):
+            img_width, img_height = pil_img.size
+            return pil_img.crop(((img_width - crop_width) // 2,
+                                (img_height - crop_height) // 2,
+                                (img_width + crop_width) // 2,
+                                (img_height + crop_height) // 2))
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +15,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+                
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            image_size_1, image_size_2 = img.size
+            if image_size_1 > image_size_2:
+                img = crop_center(img, image_size_2, image_size_2)
+                
+            else:
+                img = crop_center(img, image_size_1, image_size_1)
+                
+            img.thumbnail(output_size)
+            img.save(self.image.path)
